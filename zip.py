eb0814c11d84bad9c2b783738a71358ffcb4759e
Divya N <divyagowda99.dg@gmail.co>
2018-09-13 22:05:04 +0530

    programs

--- zip.py
+++ zip.py
@@ -1,10 +1,15 @@
-#!/usr/bin/env python
-from zipfile import *
-def main(): #{
-    file_name="shipping.zip";
-    zip_archive=zipFile(file_name,"w");
-    zip_archive.close();
-    #}
-    if(_name_=="_main_"):#{
-        main();
-        #}
+# importing required modules 
+from zipfile import ZipFile 
+  
+# specifying the zip file name 
+file_name = "divepython2.zip"
+  
+# opening the zip file in READ mode 
+with ZipFile(file_name, 'r') as zip: 
+    # printing all the contents of the zip file 
+    zip.printdir() 
+  
+    # extracting all the files 
+    print('Extracting all the files now...') 
+    zip.extractall() 
+    print('Done!')
