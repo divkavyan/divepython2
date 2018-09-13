95787220f6099c48fdc0e8712d8e059f37ed3aeb
Divya N <divyagowda99.dg@gmail.co>
2018-09-13 12:30:28 +0530

    all programs

---
+++ zip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from zipfile import *
+def main(): #{
+    file_name="shipping.zip";
+    zip_archive=zipFile(file_name,"w");
+    zip_archive.close();
+    #}
+    if(_name_=="_main_"):#{
+        main();
+        #}
